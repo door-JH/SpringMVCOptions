5639f51eed7a6073b81fb25a90a8b7be51186266
elogio.mi <elogio.mi@gmail.com>
2022-10-16 22:16:58 +0900

    comp 06

--- Spring0604Mybatis/src/main/java/info/example/mapper/MapperInterface.java
+++ Spring0604Mybatis/src/main/java/info/example/mapper/MapperInterface.java
@@ -2,8 +2,10 @@ package info.example.mapper;
 
 import java.util.List;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.context.annotation.Bean;
 
 import info.example.beans.DataBean1;
 
@@ -14,4 +16,5 @@ public interface MapperInterface {
 	
 	@Select("select data1, data2, data3 from SPRING0604_TABLE")
 	List<DataBean1> select_data();
+	
 }
